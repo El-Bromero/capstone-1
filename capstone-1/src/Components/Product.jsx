8b09bd219b1c275bb239684026f08d7c3df1f5c8
El-Bromero <brandonaromero@gmail.com>
2021-02-14 16:09:41 -0600

    Added view item details button that opens up a modal with all of the item details inside

--- capstone-1/src/Components/Product.jsx
+++ capstone-1/src/Components/Product.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Product.css'
 import { useStateValue } from './StateProvider';
+
+import Modal from 'react-modal';
+import zIndex from '@material-ui/core/styles/zIndex';
+Modal.setAppElement('#root');
 
 function Product({ name, serialNumber, price, category, quantity, productImage }) {
     
@@ -19,6 +23,8 @@ function Product({ name, serialNumber, price, category, quantity, productImage }
             },
         })
     };
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     
     return (
         <div className="product">
@@ -31,8 +37,42 @@ function Product({ name, serialNumber, price, category, quantity, productImage }
             </div>
             <div className="product-bottom">
                 <img src={productImage} alt="" />
+                <button onClick={() => setModalIsOpen(true)}>View item details</button>
                 <button onClick={addToCart}>Add to cart</button>
             </div>
+            <Modal 
+                isOpen={modalIsOpen} 
+                onRequestClose={() => setModalIsOpen(false)}
+                style={
+                    {
+                        overlay: {
+                            zIndex: '1000'
+                        },
+                        content: {
+                            color: 'black',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            // alignItems: 'center',
+                            // justifyContent: 'center',
+                            top: '150px',
+                            left: '300px',
+                            right: '300px',
+                            bottom: '250px'
+                        }
+                    }
+                }
+            >
+                <h1 className="product-modalTitle">{name}</h1>
+                <ul>
+                    <li>Price: ${price}</li>
+                    <li>Serial Number: {serialNumber}</li>
+                    <li>Category: {category}</li>
+                    <li>Quantity: {quantity}</li>
+                    <img src={productImage} alt="" className="product-img" />
+                    
+                </ul>
+                <button onClick={() => setModalIsOpen(false)} className="product-modalButton">Close</button>
+            </Modal>
         </div>
     )
 }

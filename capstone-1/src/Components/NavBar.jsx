090f0bedf39575aa67ea1a8d6e2e3c76ccd74207
El-Bromero <brandonaromero@gmail.com>
2021-02-14 18:14:32 -0600

    search bar works but it does look jank. Had issues with dropdown results giving product info so I made some work arounds. When typing item nameit has a list of suggested items with whatever user has inputted. It only shows 1 product with information though. If user is more specific then the desired product will appear

--- capstone-1/src/Components/NavBar.jsx
+++ capstone-1/src/Components/NavBar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import './NavBar.css'
+import React, { useState } from 'react';
+import './NavBar.css';
 import logo from '../Images/romeroscape_logo_crop.png';
 import { Link } from 'react-router-dom';
 import SearchIcon from "@material-ui/icons/Search";
-// import EcoIcon from '@material-ui/icons/Eco';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { useStateValue } from './StateProvider';
+
+import inventory from '../Data/inventory.json';
+import Product from './Product';
 
 function NavBar() {
     
     const [{ cart }] = useStateValue();
+
+    const [searchTerm, setSearchTerm] = useState("");
     
     return (
         <nav className="navbar">
@@ -25,37 +29,68 @@ function NavBar() {
             {/* Search Box */}
 
             <div className="navbar-search">
-                <input type="text" className="navbar-searchInput"/>
+                <input 
+                    type="text" 
+                    className="navbar-searchInput"
+                    list="names"
+                    onChange={(event) => {
+                        setSearchTerm(event.target.value);
+                    }}
+                />
+                {/* FILTER RESULTS */}
+                {inventory.filter((val)=> {
+                    if (searchTerm === "") {
+                        return val;
+                    } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                        return val;
+                    }
+    
+                }).map((val, key) => {
+                    if(searchTerm === "")
+                    {
+                        return;
+                    }
+                    return (
+                        <div className="navbar-item-name" key={key}>
+                            <Product 
+                                name={val.name} 
+                                serialNumber={val.serialNumber} 
+                                price={val.price} 
+                                category={val.category} 
+                                quantity={val.quantity} 
+                                productImage={val.productImage}
+                            />
+                            {/* <button>{val.name}</button> */}
+                            <datalist id="names">
+                                {inventory.map((val, key) => {
+                                    return <option value={val.name}/>
+                                })}
+                            </datalist>
+                        </div>
+                    )
+                })
+                }
                 <SearchIcon className="navbar-searchIcon"/>
             </div>
             {/* Links */}
             <div className="navbar-nav">
                 {/* 1st link */}
-                <Link to="/login" className="navbar-link">
+                {/* <Link to="/login" className="navbar-link">
                     <div className="navbar-option">
                         <span className="navbar-optionLineOne">Hello Brandon,</span>
                         <span className="navbar-optionLineTwo">Sign In</span>
                     </div>
-                </Link>
+                </Link> */}
 
                 {/* 2nd link */}
                 <Link to="/about-us" className="navbar-link">
                     <div className="navbar-option">
                         <span className="navbar-optionLineOne navbar-optionIcon">
-                        {/* <EcoIcon style={{ fontSize: 15 }}/> */}
                         Learn More
                         </span>
                         <span className="navbar-optionLineTwo">About Us</span>
                     </div>
                 </Link>
-
-                {/* 3rd link */}
-                {/* <Link to="/orders" className="navbar-link">
-                    <div className="navbar-option">
-                        <span className="navbar-optionLineOne">Your</span>
-                        <span className="navbar-optionLineTwo">Orders</span>
-                    </div>
-                </Link> */}
 
             </div>
             {/* Cart Icon */}
